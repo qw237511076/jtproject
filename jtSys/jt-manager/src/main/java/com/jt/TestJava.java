0016991a6f88cf20902a18fb436ad5d861236660
Mr.Li <lmlfeifei@163.com>
2019-08-22 22:39:37 +0800

    idea git使用测试

---
+++ jtSys/jt-manager/src/main/java/com/jt/TestJava.java
@@ -0,0 +1,11 @@
+package com.jt;
+
+/**
+ * @author sumail
+ * @date 2019/8/22 0022-${time}
+ */
+public class TestJava {
+    public static void main(String[] args) {
+        System.out.println("hello java");
+    }
+}
